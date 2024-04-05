d057b5c9104673460bd6644d7c992f697544929f
Mxmilu <mxmilu666@gmail.com>
2024-04-05 16:57:04 +0800

    ✨ 添加Ctrl+C安全退出

--- main.php
+++ main.php
@@ -1,7 +1,8 @@
 <?php
+use Swoole\Coroutine;
 use function Swoole\Coroutine\run;
 use function Swoole\Timer;
-
+declare(ticks=1)
 require './config.php';
 const VERSION = '1.10.3';
 const DOWNLOAD_DIR = "./cache";
@@ -19,16 +20,33 @@ $list = glob('./inc/*.php');
     }
 echo"OpenBmclApionPHP v0.0.1-dev". PHP_EOL;
 run(function()use ($config){
+    //注册信号处理器
+    function registerSigintHandler() {
+        global $timerId;
+        Swoole\Process::signal(SIGINT, function ($signo) use ($timerId) {
+            try {
+                Swoole\Timer::clear($timerId);
+                mlog("主动退出...");
+                exit;
+            } catch (\Swoole\ExitException $e) {
+                //var_dump($e->getMessage());
+                //var_dump($e->getStatus() === 1);
+                //var_dump($e->getFlags() === SWOOLE_EXIT_IN_COROUTINE);
+            }
+        });
+    }
     //获取初次Token
     $token = new token($config['CLUSTER_ID'],$config['CLUSTER_SECRET'],VERSION);
     $tokendata = $token->gettoken();
     mlog("GetToken:".$tokendata['token'],1);
     mlog("TokenTTL:".$tokendata['upttl'],1);
     //启动更新TokenTimer
+    global $timerId;
     $timerId = Swoole\Timer::tick($tokendata['upttl'], function () use ($token) {
         $tokendata = $token->gettoken();
         mlog("GetNewToken:".$tokendata['token'],1);
     });
+    registerSigintHandler();
     mlog("Timer start on ID{$timerId}",1);
     
     //下载文件列表
